15885d1cec5ef9269391ed5b89789e1411bf0080
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-06-11 14:42:37 -0300

    criação de projeto e conexão com base de dados postgresql

---
+++ src/test/java/com/example/aula_springboot/AulaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
